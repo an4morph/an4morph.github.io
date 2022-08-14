789f99abe9060c9e052e57fde48e22f549b66101
Polina <avgustin456@gmail.com>
2022-08-14 14:18:47 +0600

    about desktop sizes

--- src/components/about-block/index.tsx
+++ src/components/about-block/index.tsx
@@ -1,17 +1,17 @@
 import Photo1 from "../../assets/img/polina-1.png"
-import { Container, Job, Line, Name, Title } from "./styled"
+import { Container, HiThere, Job, Line, Name, SecondP, ThirdP, Title } from "./styled"
 
 export const AboutBlock: React.FC = () => {
   return (
     <Container>
       <Line>
         <img alt="polina" src={Photo1}/>
-        <p>
-          Hi there! I’m Polina, and I’m designer. I specialize in UI/UX design. From market research and 
+        <HiThere>
+          Hi there! I’m Polina, and I’m designer.{`\n`} I specialize in UI/UX design. From market research and 
           wireframes to mockup and testing. My advantage is that I have a great background as a front-end 
           developer. I have been developing websites using ReactJS since 2017. And this helps me to see my 
           design as a developer, to understand how long and difficult it will be to develop it
-        </p>
+        </HiThere>
       </Line>
 
       <Line>
@@ -19,12 +19,16 @@ export const AboutBlock: React.FC = () => {
           <Name>Polina Akhmetova</Name>
           <Job>UI/UX Designer | Frontend developer</Job>
         </Title>
-        <p>
+        <SecondP>
           I started designing during the pandemic in 2020. At the university, I studied the design of the 
           architectural environment, and then transferred to software engineering. Now I have found an area 
           that combines my two passions - and this is design in IT.
-        </p>
+        </SecondP>
       </Line>
+
+      <ThirdP>
+        Below on this site, you can learn more about my skills, see my portfolio and find contacts for communication
+      </ThirdP>
 
     </Container>
   )
--- src/components/about-block/styled.ts
+++ src/components/about-block/styled.ts
@@ -6,15 +6,18 @@ export const Container = styled.div`
   position: relative;
 
   & img {
-    width: 320px;
+    width: 24vw;
+    margin-top: 3vw;
+    margin-left: 6vw;
   }
 
   & p {
-    font-size: 18px;
+    font-size: 1.2vw;
     font-family: ${({ theme }) => theme.fonts.normal};
     line-height: 140%;
     letter-spacing: 0.02em;
-    color: ${Colors.webBlack}
+    color: ${Colors.webBlack};
+    white-space: pre-line;
   }
 `
 
@@ -24,18 +27,39 @@ export const Line = styled.div`
 
 export const Title = styled.div`
   font-family: ${({ theme }) => theme.fonts.primary};
+  margin-top: 3.6vw;
+  margin-left: 2vw;
 `
 
 export const Name = styled.div`
-  font-size: 50px;
+  font-size: 4vw;
   text-transform: uppercase;
-  line-height: 50px;
+  line-height: 4vw;
   white-space: nowrap;
-  margin-bottom: 16px;
+  margin-bottom: 1.2vw;
 `
 
 export const Job = styled.div`
-  font-size: 18px;
-  line-height: 18px;
+  font-size: 1.4vw;
+  line-height: 1.4vw;
   white-space: nowrap;
+`
+
+export const HiThere = styled.p`
+  width: 40vw;
+  margin-top: 6.8vw;
+  margin-left: 10vw;
+`
+
+export const SecondP = styled.p`
+  width: 100%;
+  margin-left: 10vw;
+  margin-top: 3vw;
+`
+
+export const ThirdP = styled.p`
+  width: 41vw;
+  margin-left: 10vw;
+  margin-top: 3vw;
+  margin-bottom: 3vw;
 `
