14fce99208cde68dacf124c923cdd199ff060b77
Polina <avgustin456@gmail.com>
2022-09-14 15:44:54 +0600

    reconstruction

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import styled from "styled-components"
+
 import { AboutBlock } from "./components/about-block"
 import { ContactsBlock } from "./components/contacts-block"
 import { CVBlock } from "./components/cv-block"
@@ -5,15 +7,25 @@ import { Nav } from "./components/nav"
 import { SkillsBlock } from "./components/skills-block"
 import { Container } from "./styles/App.styled"
 
+const Dev = styled.div`
+  display: flex;
+  justify-content: center;
+  font-size: 4vw;
+  height: 100vh;
+  align-items: center;
+`
+
 const App: React.FC = () => {
   return (
     <Container>
-      <Nav>
+      {/* <Nav>
         <AboutBlock />
         <SkillsBlock />
         <CVBlock />
         <ContactsBlock />
-      </Nav>
+      </Nav> */}
+
+      <Dev>Sorry, website under reconstruction</Dev>
     </Container>
   )
 }
